ca2bf0446040ecee7582feab24479d9252b71bfa
nico <nico.poulain@gmail.com>
2012-09-09 23:40:06 +0200

    Ajout de la librairie javascript utilisee sur sta_par_resp pour filtrer. GROSSE AVANCEE git add jquery.dataTables.load.js jquery.dataTables.js

---
+++ gaiabb/jquery.dataTables.load.js
@@ -0,0 +1,43 @@
+var asInitVals = new Array();
+
+jQuery(document).ready(function() {
+  var oTable = jQuery('#table-id').dataTable( {
+    "oLanguage": {
+        "sSearch": "Filtrage en direct:",
+    },
+    "bPaginate": false,
+
+        "fnDrawCallback": function ( oSettings ) {
+            if ( oSettings.aiDisplay.length == 0 )
+            {
+                return;
+            }
+             
+            var nTrs = $('#example tbody tr');
+            var iColspan = nTrs[0].getElementsByTagName('td').length;
+            var sLastGroup = "";
+            for ( var i=0 ; i<nTrs.length ; i++ )
+            {
+                var iDisplayIndex = oSettings._iDisplayStart + i;
+                var sGroup = oSettings.aoData[ oSettings.aiDisplay[iDisplayIndex] ]._aData[0];
+                if ( sGroup != sLastGroup )
+                {
+                    var nGroup = document.createElement( 'tr' );
+                    var nCell = document.createElement( 'td' );
+                    nCell.colSpan = iColspan;
+                    nCell.className = "group";
+                    nCell.innerHTML = sGroup;
+                    nGroup.appendChild( nCell );
+                    nTrs[i].parentNode.insertBefore( nGroup, nTrs[i] );
+                    sLastGroup = sGroup;
+                }
+            }
+        },
+        "aoColumnDefs": [
+            { "bVisible": false, "aTargets": [ 0 ] }
+        ],
+        "aaSortingFixed": [[ 0, 'asc' ]],
+        "aaSorting": [[ 1, 'asc' ]],
+        "sDom": 'lfr<"giveHeight"t>ip'
+    });
+} );

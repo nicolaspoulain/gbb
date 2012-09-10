5631698936e5eaa4d55293f8c8b072104b66a0f2
nico <nico.poulain@gmail.com>
2012-09-10 12:23:22 +0200

    les stages de la liste sont cliquables pour afficher les infos sur l'organisation.

--- gaiabb/jquery.dataTables.load.js
+++ gaiabb/jquery.dataTables.load.js
@@ -1,13 +1,47 @@
 var asInitVals = new Array();
 
+function fnFormatDetails ( nTr )
+{
+        var aData = oTable.fnGetData( nTr );
+            var sOut = '<table cellpadding="5" cellspacing="0" border="0" style="padding-left:50px;">';
+                sOut += '<tr><td>Rendering engine:</td><td>'+aData[2]+' '+aData[2]+'</td></tr>';
+                    sOut += '<tr><td>Link to source:</td><td>Could provide a link here</td></tr>';
+                        sOut += '<tr><td>Extra info:</td><td>And any further details here (images etc)</td></tr>';
+                            sOut += '</table>';
+                                 
+                                     return sOut;
+}
+
+
 jQuery(document).ready(function() {
+  jQuery('#table-id tbody tr').click( function () {
+    if ( oTable.fnIsOpen(this) ) {
+      oTable.fnClose( this );
+    } else {
+      var aData = oTable.fnGetData( this );
+      oTable.fnOpen( this, aData[3], "info_row" );
+    }
+  } );
+
   var oTable = jQuery('#table-id').dataTable( {
     "oLanguage": {
         "sSearch": "Filtrage en direct:",
+        "sInfo": "Affichage des modules _START_ à _END_ (sur _TOTAL_)",
     },
-        "bStateSave": true,
-  "bPaginate": false,
-        "fnDrawCallback": function ( oSettings ) {
+    "bStateSave": true,
+    "bPaginate": false,
+    "aaSortingFixed": [[ 0, 'asc' ]],
+    "aaSorting": [[ 1, 'asc' ]],
+    "bAutoWidth": false,
+    "aoColumns": [
+            { "bVisible": false },
+            null,
+            null,
+            { "bVisible": false },
+            null,
+        ],
+    "sDom": 'lfr<"giveHeight"t>ip',
+    "fnDrawCallback": function ( oSettings ) {
             if ( oSettings.aiDisplay.length == 0 )
             {
                 return;
@@ -33,17 +67,5 @@ jQuery(document).ready(function() {
                 }
             }
         },
-        "aoColumnDefs": [
-            { "bVisible": false, "aTargets": [ 0 ] }
-        ],
-        "aoColumns": [
-                { "bVisible": false },
-                null,
-                null,
-                null
-              ],
-        "aaSortingFixed": [[ 0, 'asc' ]],
-        "aaSorting": [[ 1, 'asc' ]],
-        "sDom": 'lfr<"giveHeight"t>ip'
     });
 } );
